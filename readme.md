fee5d6d2c696fdc1df2d1cb2e77c2d19dcec9f3c
Dan Leatherman <dleatherman@gmail.com>
2013-05-21 12:00:06 -0400

    add in readme and a few small changes

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Node + Backbone Library Application
+
+This is an app from a series of tutorials by [Javascript Playground](http://javascriptplayground.com) called [Building an App with Express and Backbone](http://javascriptplayground.com/blog/2013/02/express-backbone-application-part1).
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,7 @@ exports.books = {};
 exports.books.all = function(req, res) {
   db.books.find({}, function(err, books){
     if(err) return;
-    var response = {
-      books: books
-    };
-    res.json(response);
+    res.json(books);
   });
 };
 
@@ -16,9 +13,6 @@ exports.books.one = function(req, res) {
   var bookId = db.ObjectId(req.params.id);
   db.books.findOne({ '_id' : bookId }, function(err, book) {
     if(err) return;
-    var response = {
-      book: book
-    };
-    res.json(response);
+    res.json(book);
   });
 };
